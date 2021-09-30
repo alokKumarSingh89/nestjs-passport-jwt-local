6ffe6272e99ad7152e51f64eb334a851381e2d7b
ALOK SINGH <aloksingh@ALOKs-iMac.local>
2021-10-01 01:48:38 +0530

    Session done

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { LocalAuthGuard } from './auth/local-auth.guard';
+import { AuthenticatedGuard } from './auth/authenticated.guard';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  login(@Request() req): any {
+    return { msg: 'Login In!' };
+  }
+
+  @UseGuards(AuthenticatedGuard)
+  @Get('protected')
   getHello(): string {
     return this.appService.getHello();
   }
